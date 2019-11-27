8d7a88a68c2d504007c7b5aa1d0f152abb4bfc01
Romario Gustave <romagustave10@gmail.com>
2019-11-27 14:03:59 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,14 +1,11 @@
 class Person
   def initialize(name)
-    @name = name()
+    @name = name
   end 
   def name=(name)
     @name = name
   end
- 
   def name
     @name
   end
 end
- 
-beyonce = Person.new
